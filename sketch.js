98949b595bd781b227b4ec184f333da2cf4d7776
yashaswinishet <yashaswinishet@gmail.com>
2020-12-14 15:23:02 +0530

    msg

--- sketch.js
+++ sketch.js
@@ -35,6 +35,7 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+    platform1 = new Ground(150,300,200,20);
 
 }
 
@@ -61,4 +62,5 @@ function draw(){
 
     bird.display();
     platform.display();
+    platform1.display();
 }
